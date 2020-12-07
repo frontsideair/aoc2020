69456ff229c6904413cc0efb406793d40e559e51
Fatih Altinok <photonia@gmail.com>
2020-12-08 00:18:33 +0300

    Day 1

---
+++ day1/Main.hs
@@ -0,0 +1,11 @@
+year = 2020
+
+main :: IO ()
+main = do
+  file <- readFile "./input"
+  let numbers = read <$> lines file :: [Int]
+  let results = [a * b | a <- numbers, b <- numbers, a + b == year]
+  print results
+  let results = [a * b * c | a <- numbers, b <- numbers, c <- numbers, a + b + c == year]
+  print results
+  return ()
